75b87308112d005975f7d3378108bd6a56fbdf89
shekhar.shukla <shekhar.shukla@BLR1-LMAC-N03224.local>
2019-06-20 16:32:38 +0530

    heroku implementation

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 const { check, validationResult } = require('express-validator');
 
 //mongoDb connected
 connectDB();
 app.use(express.json());
-app.get('',(req,res) => {
-    res.send('api running');
-})
+// app.get('',(req,res) => {
+//     res.send('api running');
+// })
 
 //Define routes
 
 app.use('/api/user',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
+
+//Serve static content for production
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendfile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,function(){
